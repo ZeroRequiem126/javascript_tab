8ef3f15408405a5066d6a74347e4f9990aace626
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-02-16 22:01:27 +0900

    コンテンツエリアのDOM要素を取得

--- main.js
+++ main.js
@@ -14,9 +14,21 @@
     const handleClick = (e) => {
         e.preventDefault();
         console.log('Clicked');
+        
+        // クリックされたnavとそのdataを取得
+        const $this = e.target;
+        const targetVal = $this.dataset.nav;
+
+        // 対象のコンテンツをアクティブ化する
+        $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
     };
 
-    $nav[0].addEventListener('click', (e) => handleClick(e));
+    // 全nav要素に対して関数を適用・発火
+    let index = 0;
+    while(index < $nav.length) {
+        $nav[index].addEventListener('click', (e) => handleClick(e));
+        index++;
+    }
 
 
 })(); 
