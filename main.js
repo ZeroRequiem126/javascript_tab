ec3d5935dd428ee58a42fe014a709d75e5fdbb92
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-02-17 17:30:55 +0900

    コンテンツのアクティブと非表示処理を追加

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
     const $tab = $doc.getElementById('js-tab');
     const $nav = $tab.querySelectorAll('[data-nav');
     const $content = $tab.querySelectorAll('[data-content');
+    const ACTIVE_CLASS = 'is-active';
+    const navLen = $nav.length;
 
     // 初期化
     const init = () => {
@@ -13,19 +15,27 @@
     // クリックイベント
     const handleClick = (e) => {
         e.preventDefault();
-        console.log('Clicked');
         
         // クリックされたnavとそのdataを取得
         const $this = e.target;
         const targetVal = $this.dataset.nav;
 
+        // 対象外のnav,contentsをリセットする
+        let index = 0;
+        while(index < navLen) {
+            $content[index].style.display = 'none';
+            $nav[index].classList.remove(ACTIVE_CLASS);
+            index++;
+        }
+
         // 対象のコンテンツをアクティブ化する
         $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+        $nav[targetVal].classList.add(ACTIVE_CLASS);
     };
 
     // 全nav要素に対して関数を適用・発火
     let index = 0;
-    while(index < $nav.length) {
+    while(index < navLen) {
         $nav[index].addEventListener('click', (e) => handleClick(e));
         index++;
     }
